652768f286839e15fd0a27696e04bf074713d267
Simon Koch <simon.koch@tu-braunschweig.de>
2023-03-14 14:47:54 +0100

    configuration class specs and json parser

---
+++ src/main/scala/de/tubs/cs/ias/plotalyzer/Configuration.scala
@@ -0,0 +1,37 @@
+package de.tubs.cs.ias.plotalyzer
+
+import de.tubs.cs.ias.plotalyzer.plugins.{
+  PluginManagerConfiguration,
+  RemotePluginConfig
+}
+import spray.json.{DefaultJsonProtocol, JsonParser, RootJsonFormat}
+import scala.io.Source
+
+/** the object representation of the config.json
+  *
+  * @param plugin the plugin specific configuration
+  */
+case class Configuration(plugin: PluginManagerConfiguration)
+
+object Configuration extends DefaultJsonProtocol {
+
+  implicit val pluginFormat: RootJsonFormat[RemotePluginConfig] = jsonFormat2(
+    RemotePluginConfig)
+
+  implicit val pluginManagerConfigurationFormat
+    : RootJsonFormat[PluginManagerConfiguration] = jsonFormat2(
+    PluginManagerConfiguration)
+
+  implicit val configurationFormat: RootJsonFormat[Configuration] = jsonFormat1(
+    Configuration.apply)
+
+  def read(file: String): Configuration = {
+    val source = Source.fromFile(file)
+    try {
+      JsonParser(source.mkString).convertTo[Configuration]
+    } finally {
+      source.close()
+    }
+  }
+
+}
