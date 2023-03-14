77cc06a18a8f984231ec2b69a254a2c6615df315
Simon Koch <simon.koch@tu-braunschweig.de>
2023-03-14 14:48:15 +0100

    commented code

--- src/main/scala/de/tubs/cs/ias/plotalyzer/Plotalyzer.scala
+++ src/main/scala/de/tubs/cs/ias/plotalyzer/Plotalyzer.scala
@@ -6,91 +6,123 @@ import de.halcony.argparse.{
   ParsingException,
   ParsingResult
 }
-import de.tubs.cs.ias.plotalyzer.analysis.consentdialog.{
-  ConsentDialogAnalysis,
-  SpotcheckGenerator
-}
-import de.tubs.cs.ias.plotalyzer.analysis.tcfdetection.{
-  DynamicTCFDetection,
-  StaticTCFDetection,
-  TCFDetectionConf
-}
-import de.tubs.cs.ias.plotalyzer.analysis.trafficanalysis.filter.HostPathFilterTypes.{
-  EXODUS,
-  GENERIC
-}
-import de.tubs.cs.ias.plotalyzer.analysis.trafficanalysis.filter.TrafficFilter
-import de.tubs.cs.ias.plotalyzer.analysis.trafficanalysis.{
-  RequestTrackingEndpointAnalysis,
-  TrafficCollectionAnalysisConfig,
-  TrafficSummary
-}
-import de.tubs.cs.ias.plotalyzer.analysis.{
-  BasicExperimentSummary,
-  ExperimentErrorSummary,
-  FailedAnalysisSummary
-}
-import de.tubs.cs.ias.plotalyzer.database.entities.{
-  Experiment,
-  InterfaceAnalysis
-}
+import de.tubs.cs.ias.plotalyzer.database.entities.Experiment
 import de.tubs.cs.ias.plotalyzer.database.{Database, DatabaseConf}
-import de.tubs.cs.ias.plotalyzer.json.AppmiRankingListSet
-import de.tubs.cs.ias.plotalyzer.plugins.PluginManager
-import de.tubs.cs.ias.plotalyzer.utility.Time
-import spray.json.{JsArray, JsNumber, JsObject, JsString, JsValue, JsonParser}
+import de.tubs.cs.ias.plotalyzer.plugins.{
+  AnalysisReturn,
+  BasicContext,
+  JSONReturn,
+  PluginManager
+}
+import spray.json.{JsObject, JsString}
 import wvlet.log.LogSupport
-
-import java.io.{BufferedWriter, File, FileWriter}
+import java.io.File
 import scala.io.Source
 
+/** The main object handling running the plotalyzer and its plugins
+  *
+  * @author Simon Koch
+  */
 object Plotalyzer extends LogSupport {
 
+  /** cmd parser spec
+    */
   val parser: Parser = Parser(
     "plotalyzer",
     "takes a scala-appanalyzer database and generates json for further processing")
-    .addPositional("database-conf", "a json containing the database conf")
-    .addPositional("id", "the id of the experiment to analyze")
-    .addPositional(
-      "out",
-      "the path to print the result into (folder for screenshot json for data)")
-    .addPositional("plugin", "the name of the plugin to use")
-    .addDefault[ParsingResult => JsValue]("func", runPlugin, "run dem plugin")
+    .addOptional("conf",
+                 "c",
+                 "config",
+                 Some("./resources/config.json"),
+                 "the configuration file")
+    .addSubparser(PluginManager.parser)
+    .addSubparser(
+      Parser("analyze", "perform an analysis")
+        .addOptional("database-conf",
+                     "d",
+                     "database-conf",
+                     Some("./resources/db.conf"),
+                     "a json containing the database conf")
+        .addPositional("id", "the id of the experiment to analyze")
+        .addPositional("out", "the path to print the result into")
+        .addPositional("plugin", "the name of the plugin to use")
+        .addOptional("only",
+                     "o",
+                     "only",
+                     None,
+                     "only analyze apps contained in the csv or file provided")
+        .addDefault[(ParsingResult, Configuration) => Unit]("func",
+                                                            runPlugin,
+                                                            "run dem plugin"))
 
-  def runPlugin(pargs: ParsingResult): JsValue = {
-    println(PluginManager.getPlugins)
-    PluginManager.loadPlugins(pargs.getValue[String]("plugin")) match {
-      case Some(plugin) =>
-        plugin.analyze(null) match {
-          case Left(value: Exception) => throw value
-          case Right(value: JsValue)  => value
-        }
-      case None => JsObject("error" -> JsString("plugin cannot be loaded"))
+  /** based on the provided --only flags returns the set of app ids to restrict the analysis to
+    *
+    * @param str the string either pointing to a file in which each line is one app or the string itself is a csv list
+    * @return the retrieved set of app ids to restrict the analysis to
+    */
+  private def getOnly(str: String): Seq[String] = {
+    val file = new File(str)
+    if (file.exists() && file.isFile) {
+      val source = Source.fromFile(file)
+      try {
+        source.getLines().toSet.toSeq
+      } finally {
+        source.close()
+      }
+    } else {
+      str.split(",").toSeq
     }
   }
 
+  /** extracts cmd args, runs the specified plugin, and writes the results into out
+    *
+    * @param pargs the parsed command line arguments
+    */
+  def runPlugin(pargs: ParsingResult, conf: Configuration): Unit = {
+    val pluginManager = PluginManager.getPluginManager(conf.plugin)
+    pluginManager.loadPlugins(pargs.getValue[String]("plugin")) match {
+      case Some(plugin) =>
+        implicit val database: Database = getDatabaseConnection(pargs)
+        val experiment: Experiment = Experiment(pargs.getValue[String]("id"))
+        val only: Option[Seq[String]] =
+          pargs.get[OptionalValue[String]]("only").value match {
+            case Some(only) => Some(getOnly(only))
+            case None       => None
+          }
+        val context = BasicContext(experiment, only, "") // create plugin analysis context
+        plugin.analyze(context) match {
+          case Left(value: Exception) => throw value
+          case Right(ret: AnalysisReturn) =>
+            ret.write(pargs.getValue[String]("out"))
+        }
+      // if there is no plugin to be loaded we write give an error log and write an error json
+      case None =>
+        error("no plugin loaded - something went wrong")
+        JSONReturn(JsObject("error" -> JsString("plugin cannot be loaded")))
+          .write(pargs.getValue[String]("out"))
+    }
+  }
+
+  /** parses the command line args and starts the correct main
+    *
+    * @param args the cmd arguments
+    */
   def main(args: Array[String]): Unit = {
     try {
       val pargs = parser.parse(args)
-      val ret: Option[JsValue] = Option(
-        pargs.getValue[ParsingResult => JsValue]("func")(pargs))
-      ret match {
-        case Some(value) =>
-          val out = new BufferedWriter(
-            new FileWriter(new File(pargs.getValue[String]("out"))))
-          try {
-            out.write(value.prettyPrint)
-          } finally {
-            out.flush()
-            out.close()
-          }
-        case None =>
-      }
+      val conf = Configuration.read(pargs.getValue[String]("conf"))
+      pargs.getValue[(ParsingResult, Configuration) => Unit]("func")(pargs,
+                                                                     conf)
     } catch {
       case x: ParsingException => println(x.getMessage())
     }
   }
 
+  /** creates the database connection handler based on the cmd arguments
+    *
+    * @param pargs the parsed command line arguments
+    * @return the database connection handler
+    */
   private def getDatabaseConnection(pargs: ParsingResult): Database = {
     val conf = DatabaseConf.read(pargs.getValue[String]("database-conf"))
     new Database(conf.host, conf.port, conf.user, conf.pwd, conf.database)
